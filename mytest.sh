3cbd4590d20b1f379794d2c7db1bbaee65e13831
Etherion <19214485+jjziets@users.noreply.github.com>
2021-03-16 10:35:17 +0200

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -2,6 +2,6 @@
 count=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
 
 echo "start benchmark $count gpus"
-python3 benchmark_models.py -g $c&& &>/dev/null 
+python3 benchmark_models.py -g $count 
 
 echo 'benchmark end'
