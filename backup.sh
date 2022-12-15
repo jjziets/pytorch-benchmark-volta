6a31f892eab69cd7e91373276e6530ec3452f201
Etherion <19214485+jjziets@users.noreply.github.com>
2022-12-16 00:51:13 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -14,8 +14,8 @@ ssh-copy-id -i ~/.ssh/id_rsa root@192.168.47.100
 apt install pixz -y
 apt install pv -y
 
-#systemctl stop runpod
-#systemctl stop docker.socket
-#systemctl stop docker
+systemctl stop runpod
+systemctl stop docker.socket
+systemctl stop docker
 ssh root@192.168.47.100 "mkdir /mnt/user/backup/$hname"
 tar -c -I 'pixz -k -1' -f - /var/lib/docker | pv | ssh root@192.168.47.100 "cat > /mnt/user/backup/$hname/docker.tar.pixz"
